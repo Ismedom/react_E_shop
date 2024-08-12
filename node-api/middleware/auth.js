d146c48f7388f6afdc25c3b2ab36777a455e4527
RuenChanPiseth <127710222+RuenChanPiseth@users.noreply.github.com>
2024-08-12 19:32:16 +0700

    font-end fetch by using api key

--- node-api/middleware/auth.js
+++ node-api/middleware/auth.js
@@ -1,11 +1,11 @@
-const apiKeys = new Set();
+// const apiKeys = new Set();
 
-function authenticateApiKey(req, res, next) {
-  const apiKey = req.get("X-API-Key");
-  if (!apiKeys.has(apiKey)) {
-    return res.status(401).json({ error: "Invalid API key" });
-  }
-  next();
-}
+// function authenticateApiKey(req, res, next) {
+//   const apiKey = req.get("X-API-Key");
+//   if (!apiKeys.has(apiKey)) {
+//     return res.status(401).json({ error: "Invalid API key" });
+//   }
+//   next();
+// }
 
-module.exports = { authenticateApiKey, apiKeys };
+// module.exports = { authenticateApiKey, apiKeys };
