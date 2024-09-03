9a4214acb038f6cda4ed115918e53177d9763cce
RuenChanPiseth <127710222+RuenChanPiseth@users.noreply.github.com>
2024-09-03 17:31:55 +0700

    add welcome to api

--- node-api/middleware/authenticateApiKey.js
+++ node-api/middleware/authenticateApiKey.js
@@ -4,12 +4,14 @@ const ApiKey = require("../models/apiKey");
 async function authenticateApiKey(req, res, next) {
     //
     const apiKey = req.get("apiKey");
+    if (!apiKey) return res.json({ message: "required apikey first!" });
+
     const validKey = await ApiKey.findOne({ key: apiKey });
-    if (!validKey) return res.status(401 || 400).json({ error: "Invalid API key" });
-    //
+
+    if (!validKey) return res.status(401).json({ error: "Invalid API key" });
+
     validKey.lastUsed = new Date();
     await validKey.save();
-    //
     next();
 }
 
